8c2640eb825a95a0dde8ab7e1dd4f31a0f1d3891
Hiep Nguyen <71130020+NguyenHoangHiep28@users.noreply.github.com>
2021-08-12 21:34:54 +0700

    make entered characters auto remove when cancel editing

--- Practice/public/asset/js/main.js
+++ Practice/public/asset/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     const btn = $('.edit-btn');
+    var length = 0;
     btn.click(function () {
         const current_btn = $(this);
         const id = $(this).attr("itemid");
@@ -14,11 +15,17 @@ $(document).ready(function () {
         input.val(tmpStr);
         if (current_btn.html() !== "Cancel") {
             current_btn.html('Cancel');
-            input.val(tmpStr);
+            //get before edited length
+            length = tmpStr.length;
         }else {
             current_btn.html('<i class="fas fa-edit"></i> Edit');
             input.prop('disabled', true);
             saveBtn.css("display", "none");
+            //get entered characters number when editing
+            var sub = tmpStr.length - length;
+            //remove those characters if not continue editing
+            tmpStr = tmpStr.substr(0, tmpStr.length-sub);
+            input.val(tmpStr);
         }
     });
 });
